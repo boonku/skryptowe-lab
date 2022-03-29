c6cbcd4de4de5c6ce522fb7e18a5f58afddff413
Bonku <bak.szymon01@gmail.com>
2022-03-29 20:48:25 +0200

    fix L2.1
    Fix ignorefirst and ignorelast to ignore characters in line rather than
    specified number of lines. Delimiter and separator now has default value
    and separator effects output.

--- lista2/Process.java
+++ lista2/Process.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Process {
+    // argument names
     public static final String IGNORE_FIRST = "ignorefirst";
     public static final String IGNORE_LAST  = "ignorelast";
     public static final String DELIMITER    = "delimiter";
@@ -10,6 +11,9 @@ public class Process {
     public static final String SELECT       = "select";
 
     public static final String PROJECT_ARG_DELIMITER = ",";
+    public static final String PROJECT_DELIMITER = "\t";
+    private static final String DEFAULT_DELIMITER = ",";
+    private static final String DEFAULT_SEPARATOR = "\t";
 
     // returns string value of given argument
     public static String getValue(List<String> args, String argument) {
@@ -53,37 +57,35 @@ public class Process {
                                            int ignoreFirst,
                                            int ignoreLast,
                                            String delimiter,
-                                           String separator,
                                            String select) {
         return lines.stream()
-                .skip(ignoreFirst)
-                .limit(lines.size() - ignoreLast)
+                .map(line -> line.substring(ignoreFirst, line.length() - ignoreLast))
                 .map(line -> line.replaceAll(delimiter, "\t"))
-                .map(line -> line.replaceAll("\t", separator))
                 .filter(line -> line.contains(select))
                 .collect(Collectors.toList());
     }
 
     public static void displayColumns(List<String> lines, List<Integer> project, String separator) {
+        // if project arg is empty, get all columns
         if (project.isEmpty()) {
-            int numberOfColumns = lines.get(0).split(separator).length;
+            int numberOfColumns = lines.get(0).split(PROJECT_DELIMITER).length;
             for (int i = 0; i < numberOfColumns; i++) {
                 project.add(i + 1);
             }
         }
         for (String line: lines) {
             StringBuilder stringBuilder = new StringBuilder();
-            List<String> columns = List.of(line.split(separator));
+            List<String> columns = List.of(line.split(PROJECT_DELIMITER));
             for (Integer index: project) {
                 if (index - 1 < columns.size()) {
                     stringBuilder.append(columns.get(index - 1));
-                    stringBuilder.append("\t");
+                    stringBuilder.append(separator);
                 }
             }
             if (stringBuilder.length() == 0) {
                 System.exit(1);
             } else {
-                stringBuilder.setLength(stringBuilder.length() - 1);
+                stringBuilder.setLength(stringBuilder.length() - separator.length());
             }
             System.out.println(stringBuilder);
         }
@@ -91,23 +93,27 @@ public class Process {
 
     public static void main(String[] args) {
         List<String> systemArgs = List.of(args);
+
         int ignoreFirst = getIgnoreValue(getValue(systemArgs, IGNORE_FIRST));
         int ignoreLast = getIgnoreValue(getValue(systemArgs, IGNORE_LAST));
         String delimiter = getValue(systemArgs, DELIMITER);
+        delimiter = delimiter == null ? DEFAULT_DELIMITER : delimiter;
         String separator = getValue(systemArgs, SEPARATOR);
+        separator = separator == null ? DEFAULT_SEPARATOR : separator;
         List<Integer> project = getProjectValues(getValue(systemArgs, PROJECT));
         String select = getValue(systemArgs, SELECT);
-        if (select == null) {
-            select = "";
-        }
+        select = select == null ? "" : select;
+
         List<String> lines = readLines();
-        if (lines.isEmpty()) {
-            System.exit(2);
-        }
-        lines = filterLines(lines, ignoreFirst, ignoreLast, delimiter, separator, select);
-        if (lines.isEmpty()) {
-            System.exit(1);
-        }
+        ifEmptyExitWithCode(lines, 2);
+        lines = filterLines(lines, ignoreFirst, ignoreLast, delimiter, select);
+        ifEmptyExitWithCode(lines, 1);
         displayColumns(lines, project, separator);
+    }
+
+    private static void ifEmptyExitWithCode(List<String> lines, int code) {
+        if (lines.isEmpty()) {
+            System.exit(code);
+        }
     }
 }
