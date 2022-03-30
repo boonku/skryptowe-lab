61ac1e7847124529a20457a55f4f7c709a0a8d6d
Bonku <bak.szymon01@gmail.com>
2022-03-30 21:47:38 +0200

    update L2.4 .py and .sh

--- lista2/max-avg.py
+++ lista2/max-avg.py
@@ -5,37 +5,51 @@ if len(sys.argv) < 3:
     print('Too few arguments given.')
     exit(1)
 
+# get arguments
 extension = sys.argv[1]
 try:
     number = int(sys.argv[2])
 except ValueError:
     print("First argument should be extension and second number")
     exit(1)
-
+# run Paths to get all files
 files = subprocess.check_output(
     'java Paths.java -R -s', shell=True, universal_newlines=True)
+
+# strip indentention from stdout
 files = files.splitlines()
 files = [file.strip() for file in files]
 files = '\n'.join(files)
-output = subprocess.check_output(
-    f'java Process.java --select={extension} --project=1,2', shell=True, universal_newlines=True, input=files)
 
-files = [file.split() for file in output.splitlines()]
-files = {file[0]: file[1] for file in files}
-files = dict(
-    sorted(files.items(), key=lambda item: int(item[1]), reverse=True))
+# run Process to get files with given extension, returns name, size, 'bytes'
+result = subprocess.run(
+    f'java Process.java --select={extension} --project=1,2', shell=True, universal_newlines=True, input=files, stdout=subprocess.PIPE)
+if result.returncode == 1:
+    print(f'No files with \'{extension}\' extension found.')
+    exit(1)
+
+output = result.stdout
+
+files = [file.split('\t') for file in output.splitlines()]
+files = [[file[0], file[1]] for file in files]
+files = sorted(files, key=lambda item: int(item[1]), reverse=True)
 
 data = ''
-for k, v in files.items():
-    data += f'{k} {v}\n'
+for k, v in files:
+    data += f'{k} {v} bytes\n'
 
-print(f'{number} Biggest file(/s) with "{extension}" extension:')
-subprocess.run(
-    f'java Head.java --lines={number}', shell=True, encoding='ascii', input=data)
+# output n files with given extension and their size
+print(f'{number} biggest file(/s) with \'{extension}\' extension:')
+code = subprocess.run(
+    f'java Head.java --lines={number}', encoding='utf-8', shell=True, input=data)
 
-print(f'\nAverage size of files with "{extension}" extension: ', end='')
+if (code.returncode != 0):
+    exit(1)
+
+print(f'\nAverage size of files with \'{extension}\' extension: ', end='')
+# get average size of file
 sum = 0
-for val in files.values():
+for _, val in files:
     sum += int(val)
 avg = sum / len(files)
 print(f'{avg:.2f} bytes')
--- lista2/max_mean.sh
+++ lista2/max_mean.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+re='^[0-9]+$'
+if ! [[ $2 =~ $re ]]; then
+    echo "$2 is not a number"
+    exit 1
+fi
+# get all files, select the ones with given extension and sort them by size, then store in temp file
+# temp file will be used to calculate average with awk
 java Paths.java -R -s | sed "s/^\t*//g" | java Process.java --select=$1 | sort -t$'\t' -nrk2 > tmp
-awk '{ total += $2 } END { print total/NR }' tmp
+echo "$2 biggest files with '$1' extension:"
 java Head.java --lines=$2 < tmp
+if [ ! $? -eq 0 ]; then
+    rm -f tmp
+    exit 1
+fi
+
+echo -e "\nAverage size of files with '$1' extension in bytes: \c"
+# calculate average, add up second column and in the end print sum divised by number of rows
+awk '{ total += $2 } END { print total/NR }' tmp
+
+# delete temp file
 rm -f tmp
