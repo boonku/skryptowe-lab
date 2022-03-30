3da5f0294f282ae1a668154ece7d36d9219c2095
Bonku <bak.szymon01@gmail.com>
2022-03-30 22:57:22 +0200

    add working L2.5 which splits arguments by '='

--- lista2/save.sh
+++ lista2/save.sh
@@ -1,26 +1,28 @@
 #!bin/bash
 
-src=./
-dst=
+src_dir=.
+dst_dir=
 
-if [ $1 = "--src" ]; then
-    shift
-    $src=$1
+if [[ $1 == *--src=* ]]; then
+    src_args=($(echo $1 | tr "=" "\n"))
+    src_dir=${src_args[1]}
     shift
 fi
 
-if [ $1 = "--dst" ]; then
-    shift
-    dst=$1
+if [[ $1 == *--dst=* ]]; then
+    dst_args=($(echo $1 | tr "=" "\n"))
+    dst_dir=${dst_args[1]}
 else
+    echo "Destination directory name not given"
     exit 2
 fi
-
-echo "dst = $dst"
 
 # if dir doesnt exist, make one
 if [ ! -d $dst ]; then
     mkdir $dst
 fi
+
 # copy all files from soruce to dst, non recursive
-cp $src* "$dst/"
+cp -r ./$src_dir/. ./$dst_dir
+
+echo "Copied contens to $dst_dir/"
