296fe409340df36165c5abcf42686b98e680a0cd
Szymon Bak <bak.szymon01@gmail.com>
2022-04-16 18:30:43 +0200

    add L4.2

---
+++ lista4/zad2/MultipleAccumulate.py
@@ -0,0 +1,14 @@
+from functools import reduce
+
+class MultipleAccumulate():
+    def __init__(self, data_list, *args):
+        self.data_list = data_list
+        self.accumulate_functions = args
+
+    def get_data(self):
+        index = 1
+        for func in self.accumulate_functions:
+            if func.__name__ == '<lambda>':
+                func.__name__ = f'lambda{index}'
+                index += 1
+        return {func.__name__:reduce(func, self.data_list) for func in self.accumulate_functions}
